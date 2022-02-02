4830cf353acff6410e14e85a5df9d72902592d1e
ganeaFunpay <ganea.funpay@gmail.com>
2022-02-02 15:55:14 +0200

    Added PopConfirmation

--- HeroesApp/Shared/Hero.cs
+++ HeroesApp/Shared/Hero.cs
@@ -1,12 +1,24 @@
-﻿namespace HeroesApp.Shared
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HeroesApp.Shared
 {
     public class Hero
     {
         public int Id { get; set; } = 0;
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
+        [Required]
         public string HeroName { get; set; }
+
         public Comic? Comic { get; set; }
+
+        [Required]
         public int ComicId { get; set; } = 0;
 
     }
